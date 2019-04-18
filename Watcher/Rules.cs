c7bcb0d15ee28745a71c9a7791b14508ddcd6292
RedLeonFire <RedLeonFire@yandex.by>
2019-04-18 18:30:38 +0300

    Added Work Window

---
+++ Watcher/Rules.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Windows.Controls;
+
+namespace Watcher
+{
+    //класс проверкикорректного ввода значений процентов
+    public class RulesForPersent : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int val;
+            try
+            {
+                val = int.Parse((string)value);
+            }
+            catch
+            {
+                return new ValidationResult(false, "Недопустимые символы. Или значение больше " + int.MaxValue);
+            }
+
+            if (val < 0 || val > 100)
+                return new ValidationResult(false, "Выход за границы диапазона от 0 до 100 процентов");
+            else
+                return new ValidationResult(true, null);
+
+        }
+    }
+
+    //проверка корректного ввода целочисленных параметров
+    public class RulesForInteger : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int val;
+            try
+            {
+                val = int.Parse((string)value);
+            }
+            catch
+            {
+                return new ValidationResult(false, "Недопустимые символы. Или значение больше " + int.MaxValue);
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+
+    //проверка корректного ввода IP адреса
+    public class RulesForIp : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            IPAddress val;
+
+            if ((string)value == "")
+                return new ValidationResult(true, null);
+
+            try
+            {
+                val = IPAddress.Parse((string)value);
+            }
+            catch
+            {
+                return new ValidationResult(false, $"Недопустимое значение IP");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+
+    //проверка корректного ввода порта
+    public class RulesForPort : ValidationRule
+    {
+        static readonly int bottomLimit = 1;
+        static readonly int topLimit = 65535;
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int val;
+            try
+            {
+                val = int.Parse((string)value);
+                if (val < bottomLimit || val > topLimit)
+                    throw new Exception();
+            }
+            catch
+            {
+                return new ValidationResult(false, $"Недопустимое значение порта. Диапазон [{bottomLimit}; {topLimit}]");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ Watcher/WorkWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Watcher
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWindow.xaml
+    /// </summary>
+	public partial class WorkWindow : Window
+    {
+        Thread backgroundThread;
+
+        public WorkWindow()
+        {
+            InitializeComponent();
+           // DataContext = new ViewModel();
+            backgroundThread = null;
+            btnStop.IsEnabled = false;
+        }
+
+        private void StartApplication(object sender, RoutedEventArgs e)
+        {
+           // (DataContext as ViewModel).UseGoodProcesses = UseGoodProc.IsChecked;
+            if (backgroundThread == null)
+            {
+               // backgroundThread = new Thread(new ThreadStart((DataContext as ViewModel).StartScanning));
+                backgroundThread.Start();
+                btnStart.IsEnabled = false;
+                btnStop.IsEnabled = true;
+                Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void StopThread(object sender, RoutedEventArgs e)
+        {
+            if (backgroundThread != null)
+            {
+                backgroundThread.Abort();
+                backgroundThread = null;
+                btnStop.IsEnabled = false;
+                btnStart.IsEnabled = true;
+            }
+        }
+
+        private void OpenProcessesWindow(object sender, RoutedEventArgs e)
+        {
+            ////WindowViewProcesses processWindow = new WindowViewProcesses(DataContext as ViewModel);
+            //processWindow.Owner = this;
+
+            //processWindow.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (backgroundThread != null)
+                backgroundThread.Abort();
+        }
+    }
+
+    public class ShowMessageCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            ((Window)parameter).Visibility = Visibility.Visible;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
