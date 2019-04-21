8c629ee5309741bd020920ee3728287163f97d7e
RedLeonFire <RedLeonFire@yandex.by>
2019-04-21 17:14:48 +0300

    Upload GoodProcess manager

--- Watcher/Managers/ProcessManager.cs
+++ Watcher/Managers/ProcessManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,53 +10,69 @@ namespace Watcher
 {
     class ProcessManager
     {
-        //public void AddAllSystemProcess()
-        //{
-        //    GoodProcess.Clear();
+        private string _processesFile;
+        private SortedSet<string> _goodProcess;
+        private LoggerManager _logger;
 
-        //    foreach (System.Diagnostics.Process proc in System.Diagnostics.Process.GetProcesses())
-        //        GoodProcess.Add(proc.ProcessName);
-        //}
+        public ProcessManager(string procFile, LoggerManager logger)
+        {
+            _goodProcess = new SortedSet<string>();
 
-        //public void DeletedSelectProcess()
-        //{
-        //    try
-        //    {
-        //        GoodProcess.RemoveAt(IndexSelectProcess);
-        //    }
-        //    catch { }
-        //}
+            _processesFile = procFile;
+            _logger = logger;
+        }
 
-        //public void DeleteAllProcesses()
-        //{
-        //    GoodProcess.Clear();
-        //}
+        public void AddAllSystemProcess()
+        {
+            _goodProcess.Clear();
 
-        //public void AddProcess(string process)
-        //{
-        //    GoodProcess.Add(process);
-        //}
+            foreach (Process proc in Process.GetProcesses())
+                AddProcess(proc.ProcessName);
+        }
 
-        //public void LoadGoodProcessesWithFile()
-        //{
-        //    if (File.Exists(ProcessesFilePath))
-        //    {
-        //        foreach (string proc in File.ReadAllLines(ProcessesFilePath, Encoding.Default))
-        //        {
-        //            GoodProcess.Add(proc);
-        //        }
-        //    }
-        //}
+        public void DeletedSelectProcess(string name)
+        {
+            if (_goodProcess.Contains(name))
+                _goodProcess.Remove(name);
+        }
 
-        //public void SaveGoodProcessesInFile()
-        //{
-        //    StreamWriter sw = new StreamWriter(ProcessesFilePath, true);
-        //    foreach (string proc in GoodProcess)
-        //        sw.WriteLine(proc);
-        //    sw.Close();
+        public void DeleteAllProcesses()
+        {
+            _goodProcess.Clear();
+        }
 
-        //    File.SetAttributes(ProcessesFilePath, FileAttributes.Hidden);
-        //}
+        public void AddProcess(string name)
+        {
+            _goodProcess.Add(name);
+        }
+
+        private void LoadGoodProcessesWithFile()
+        {
+            if (File.Exists(_processesFile))
+            {
+                foreach (string proc in File.ReadAllLines(_processesFile, Encoding.Default))
+                    AddProcess(proc);
+            }
+        }
+
+        public void SaveGoodProcessesInFile()
+        {
+            using (var fs = new FileStream(_processesFile, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    foreach (var name in _goodProcess)
+                        sw.WriteLine(name);
+                }
+            }
+
+            File.SetAttributes(_processesFile, FileAttributes.Hidden);
+        }
+
+        public void CheckSystemProcess()
+        {
+            foreach
+        }
 
         //private string CheckSystemProcesses()
         //{
