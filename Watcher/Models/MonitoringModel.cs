1e12e918c41a92aa600dcb911577762c9471a132
RedLeonFire <RedLeonFire@yandex.by>
2019-04-22 21:14:37 +0300

    Refactoring

--- Watcher/Models/MonitoringModel.cs
+++ Watcher/Models/MonitoringModel.cs
@@ -30,7 +30,6 @@ namespace Watcher
         public int IndexSelectProcess { get; set; }
         public bool UseGoodProcesses { get; set; } = false;
 
-        private SortedSet<string> goodProcessesSet;
         private SystemCharacterWatcher _cpuWatcher, _ramWatcher, _diskWatcher, _networkWatcher;
 
         private Loader _loader;
@@ -77,10 +76,10 @@ namespace Watcher
             _loggerManager.Start();
             _configManager.UploadSettingsCounter();
 
-            _cpuWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.CPUSectionName], _loggerManager, "%");
-            _ramWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.RAMSectionName], _loggerManager, "%");
-            _diskWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.DiskSectionName], _loggerManager, "%");
-            _networkWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.NetworkSectionName], _loggerManager, " kbyte/sec");
+            _cpuWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.CPUSectionName], _loggerManager, "%", _serverManager);
+            _ramWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.RAMSectionName], _loggerManager, "%", _serverManager);
+            _diskWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.DiskSectionName], _loggerManager, "%", _serverManager);
+            _networkWatcher = new SystemCharacterWatcher(_configManager.SettingsCounters[ConfigurationManager.NetworkSectionName], _loggerManager, " kbyte/sec", _serverManager);
             _runScan = true;
 
             ThreadPool.QueueUserWorkItem(Scanning);
